4959894577df7754f6871457c47af520d20b0029
mathbike <mathbike8@gmail.com>
2022-02-27 23:58:50 +0000

    d

--- git.sh
+++ git.sh
@@ -2,4 +2,26 @@
 
 # git commands
 
+# initialize a repository
+git init
+# clone a repository
+git clone <repository>
+# staging changes
+git add <path>
+# add all
+git add .
+# remove changes
+git reset <path>
+# status
+git status
+# commit
+git commit
+
+git init
+git add .
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/mathbike/commands.git
+git push -u origin main
+
 
