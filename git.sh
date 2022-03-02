9b9c4511595996ee15000de6dbe7892134d70406
mathbike <mathbike8@gmail.com>
2022-03-02 03:28:20 +0000

    update

--- git.sh
+++ git.sh
@@ -16,23 +16,21 @@ git reset <path>
 git status
 # commit
 git commit
-
+# init repo from cli
 git init
 git add .
 git commit -m "first commit"
 git branch -M main
 git remote add origin https://github.com/mathbike/commands.git
 git push -u origin main
-
+# config alias push
 config add .xinitrc
 config commit -m "d"
 config push
-
 # change url of origin
 git remote set-url origin new.git.url/here
 # change upstream of config origin
 config push --set-upstream "origin"
-
 # setting up new machine
 git clone --separate-git-dir=$HOME/.dotfiles https://github.com/mathbike/dotfiles.git ~
 # or create temp dir
