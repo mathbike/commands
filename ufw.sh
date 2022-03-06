de61cc9908a7984a11b7a3a2ab099d43795ba019
mathbike <mathbike8@gmail.com>
2022-03-06 22:23:51 +0000

    update

---
+++ ufw.sh
@@ -0,0 +1,17 @@
+# ufw commands
+
+# enable / disable
+sudo ufw enable
+sudo ufw disable
+# reset to default configuration
+sudo ufw reset
+# 
+sudo ufw default deny incoming
+sudo ufw default allow outgoing
+# status
+sudo ufw status
+sudo ufw status verbose
+sudo uwf status numbered
+
+
+
