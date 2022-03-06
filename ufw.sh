1642decb6e4e2b3b9b0fda3627baea7e2c547bed
mathbike <mathbike8@gmail.com>
2022-03-06 22:37:40 +0000

    update

--- ufw.sh
+++ ufw.sh
@@ -5,13 +5,20 @@ sudo ufw enable
 sudo ufw disable
 # reset to default configuration
 sudo ufw reset
-# 
+# change default settings
 sudo ufw default deny incoming
 sudo ufw default allow outgoing
 # status
 sudo ufw status
 sudo ufw status verbose
 sudo uwf status numbered
-
-
+# allow incoming ssh connections
+sudo ufw allow ssh
+sudo ufw allow 22
+# allow http
+sudo ufw allow http
+sudo ufw allow 80
+# allow https
+sudo ufw allow https
+sudo ufw allow 443
 
